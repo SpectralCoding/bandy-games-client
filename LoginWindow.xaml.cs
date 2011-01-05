72be32a3e84b39b855798b3e83b431e53b8292c0
Caesar Kabalan <Dandelock@gmail.com>
2011-01-05 11:57:46 +0000

    Client: Login window finished, mostly XAML/WPF experimentation. Basic layout so far. Next step: Create Account page.
    git-svn-id: file:///content/svn/csharp/bandy/BandyClient@12 b4a9a19a-b4d8-4a93-b48d-c161a4a7b3ea

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BandyClient {
+	/// <summary>
+	/// Interaction logic for LoginWindow.xaml
+	/// </summary>
+	public partial class LoginWindow : Window {
+		public LoginWindow() {
+			InitializeComponent();
+			// Allow dragging anywhere on the window.
+			//MouseLeftButtonDown += delegate { DragMove(); };
+		}
+
+		private void ForgotPasswordLbl_MouseUp(object sender, MouseButtonEventArgs e) {
+			throw new NotImplementedException();
+		}
+
+		private void CreateAccountLbl_MouseUp(object sender, MouseButtonEventArgs e) {
+			throw new NotImplementedException();
+		}
+
+		private void CloseBtn_Click(object sender, RoutedEventArgs e) {
+			Close();
+		}
+
+		private void LoginCmd_Click(object sender, RoutedEventArgs e) {
+			throw new NotImplementedException();
+		}
+	}
+}
